12f1abdbec80a3624ca20f74b44482c41d574db7
cmo <christopher.osborne@glasgow.ac.uk>
2026-06-26 12:11:54 +0100

    Restore time when restarting for TI problem, to give easy consistency

--- source/ProblemGenerator/PureCoronalCondensationMhdRestart.cpp
+++ source/ProblemGenerator/PureCoronalCondensationMhdRestart.cpp
@@ -111,10 +111,15 @@ static void initial_conditions(Simulation& sim, const YAML::Node& config) {
     yakl::SimpleNetCDF restart;
     restart.open(restart_path, yakl::NETCDF_MODE_READ);
     restart.read(Q0, "Q");
+    f64 current_time = 0.0_fp;
+    nc.read(current_time, "time");
 
     if (Q0.extent(0) != n_hydro) {
         throw std::runtime_error(fmt::format("Inconsistency between number of variables for fluid ({}) and in restart file ({})", Fluid::num_vars, Q0.extent(0)));
     }
+
+    sim.out_cfg.prev_output_time = current_time;
+    sim.time = current_time;
 
     dex_parallel_for(
         FlatLoop<3>(sz.zc, sz.yc, sz.xc),
